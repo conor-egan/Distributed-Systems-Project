e4a108acba2200c850196cd853c445ee8ebd0f50
conor-egan <egan.conor1@Gmail.com>
2017-04-19 11:34:34 +0100

    fixed product page

--- Distributed Systems Project-ejb/build/generated-sources/ap-source-output/DB_Entities/Customer_.java
+++ Distributed Systems Project-ejb/build/generated-sources/ap-source-output/DB_Entities/Customer_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-19T10:52:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-19T11:22:07")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
