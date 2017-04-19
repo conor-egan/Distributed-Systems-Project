c8528579dd0afc4fe52947e6eada18e3d9051e3b
conor-egan <egan.conor1@Gmail.com>
2017-04-19 10:54:35 +0100

    individual product pages working

--- Distributed Systems Project-ejb/build/generated-sources/ap-source-output/DB_Entities/Product_.java
+++ Distributed Systems Project-ejb/build/generated-sources/ap-source-output/DB_Entities/Product_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-18T22:07:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-19T10:52:27")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
---
+++ Distributed Systems Project-war/src/java/ManagedBeans/IndividualProduct.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManagedBeans;
+
+import DB_Entities.Product;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Conor
+ */
+@Named (value = "individualProduct")
+@SessionScoped
+public class IndividualProduct implements Serializable {
+
+    private Product product;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public String setProduct(Product product) {
+        this.product = product;
+        return "product_page";
+    }
+    
+    public int getID(){
+        return product.getProductId();
+    }
+    public String getName(){
+        return product.getProductName();
+    }    
+
+    public String getAuthor(){
+        return product.getProductAuthor();
+    }
+
+    public String getDescription(){
+        return product.getProductDescription();
+    }
+    
+    public int getPrice(){
+        return product.getProductPrice();
+    }
+    
+    public int getStock(){
+        return product.getProductStock();
+    }
+    
+    public String getImage(){
+        return product.getProductImage();
+    }
+
+
+
+}
